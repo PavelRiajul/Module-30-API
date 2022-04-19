09d45bf139bb3231050def2dfc5c2ae4d6994ff1
Riajul Islam <riajulpislam@gmail.com>
2022-04-19 13:36:05 +0600

    data load

--- api.js
+++ api.js
@@ -14,5 +14,13 @@ function post(){
     .then(json => displayUsers(json))
 }
 function displayUsers(json){
-    console.log(json)
+    const ul = document.getElementById('users')
+    
+    for(const user of json){
+        console.log(user)
+        const li = document.createElement('li')
+        li.innerText = `name: ${user.title} body: ${user.body} `
+        ul.appendChild(li);
+
+    }
 }
